ef3661906ce4a9ad22aadb458ee85abd1dcfc53e
richardle17 <richardle17@yahoo.com>
2021-01-31 22:05:46 -0800

    prelude

--- whiteboarded/206_reverse_linked_list.py
+++ whiteboarded/206_reverse_linked_list.py
@@ -10,6 +10,8 @@
 # A linked list can be reversed either iteratively or recursively. Could you implement both?
 
 # Definition for singly-linked list.
+
+
 class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
@@ -18,7 +20,7 @@ class ListNode(object):
 
 class Solution(object):
 
-    # start at the head, essentially flip the direction of next to previous
+    # start at the head, essentially flip the direction of next to previous and then increment through list
     def reverseList(self, head):
         """
         :type head: ListNode
@@ -27,14 +29,12 @@ class Solution(object):
         curr_node = head
         prev_node = None
 
-        while curr_node:                    # while the current node is not null
-            next = curr_node.next           # we keep track of the next node so we can iterate at the end
-            curr_node.next = prev_node      # point the direction of the node to the previous node (to reverse the direction)
-            prev_node = curr_node           # set the prev node to current node (increments the head of new list)
-            curr_node = next                # move onto the next node
-        return prev_node                    # previous is the head of the reversed linked list
-
-
+        while curr_node:  # while the current node is not null
+            next = curr_node.next  # we keep track of the next node so we can iterate at the end
+            curr_node.next = prev_node  # point the direction of the node to the previous node (to reverse the direction)
+            prev_node = curr_node  # set the prev node to current node (increments the head of new list)
+            curr_node = next  # move onto the next node
+        return prev_node  # previous is the head of the reversed linked list
 
     def reverse_list_recursive(self, head, prev=None):
         if not head:
